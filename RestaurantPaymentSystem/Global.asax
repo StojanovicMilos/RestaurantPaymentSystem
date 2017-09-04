c9500ef2f2f0c581ed34a442166334b98346048e
stojke <s.milos@sbb.rs>
2017-09-04 20:16:57 +0200

    Add project files.

---
+++ RestaurantPaymentSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestaurantPaymentSystem.MvcApplication" Language="C#" %>
---
+++ RestaurantPaymentSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantPaymentSystem.Startup))]
+namespace RestaurantPaymentSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
